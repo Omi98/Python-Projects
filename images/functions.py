89b365ac27eaadf3c5e9b397901358b47e414255
Omama K <omamak1998@gmail.com>
2024-02-28 19:25:33 +0300

    images status

--- images/functions.py
+++ images/functions.py
@@ -11,7 +11,7 @@ global image_list
 def create_window():
     myWindow = Tk()
     myWindow.title("Image Viewer")
-    myWindow.geometry("500x500")
+    #myWindow.geometry("500x500")
     myWindow.configure(background="#f5ebe0")
     return myWindow
 
@@ -53,6 +53,10 @@ def loadImages(myWindow):
     image5 = ImageTk.PhotoImage(thumbnail_img5)
 
     image_list = [image1, image2, image3, image4, image5]
+
+    # create label to show status of images
+    status = Label(myWindow, text="Image 1 of " + str(len(image_list)), border=1, relief=SUNKEN, anchor=E)
+    status.grid(row=2, column=0, columnspan=3, sticky=W+E)
     # create a label, set it to 1st image
     myLabel = Label(myWindow, image=image1)
     myLabel.grid(row=0, column=0, columnspan=3)
@@ -60,19 +64,19 @@ def loadImages(myWindow):
 
 def prevButton(myWindow):
     prev_Button = Button(myWindow, text="<<", state=DISABLED)
-    prev_Button.grid(row=1, column=0)
+    prev_Button.grid(row=1, column=0, pady=10)
 
 
 def exitButton(myWindow):
     exit_Button = Button(myWindow, text="Exit Viewer", command=myWindow.quit)
-    exit_Button.grid(row=1, column=1)
+    exit_Button.grid(row=1, column=1, pady=10)
     #myButton.place(x=270, y=150)
 
 
 def nextButton(myWindow):
     # current image index is 1 (will be called as 1 - 1 = 0)
     next_Button = Button(myWindow, text=">>", command=lambda:next(myWindow, 1))
-    next_Button.grid(row=1, column=2)
+    next_Button.grid(row=1, column=2, pady=10)
 
 
 def next(myWindow, image_num):
@@ -99,11 +103,14 @@ def next(myWindow, image_num):
     else:
         # update next button - define again
         next_Button = Button(myWindow, text=">>", command=lambda:next(myWindow, image_num+1))
-
     
     myLabel.grid(row=0, column=0, columnspan=3)  # show updated image 
     next_Button.grid(row=1, column=2)  # show >> button
     prev_Button.grid(row=1, column=0)  # show << button
+
+    # update status
+    status = Label(myWindow, text="Image " + str(image_num+1) + " of " + str(len(image_list)), border=1, relief=SUNKEN, anchor=E)
+    status.grid(row=2, column=0, columnspan=3, sticky=W+E)
 
 
 def back(myWindow, image_num):
@@ -125,6 +132,10 @@ def back(myWindow, image_num):
     myLabel.grid(row=0, column=0, columnspan=3)
     next_Button.grid(row=1, column=2)
     prev_Button.grid(row=1, column=0)
+
+    # update status
+    status = Label(myWindow, text="Image " + str(image_num-(-1)) + " of " + str(len(image_list)), border=1, relief=SUNKEN, anchor=E)
+    status.grid(row=2, column=0, columnspan=3, sticky=W+E)
 
 
 def main():
